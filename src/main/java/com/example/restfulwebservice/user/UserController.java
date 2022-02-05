b84619336e3c731a20e538da8c926d9d8add2772
LEE_CHANG_HUN <chlee@kimzipsa.co.kr>
2022-02-05 22:43:42 +0900

    Spring AOP를 이용한 Exception Handling

--- src/main/java/com/example/restfulwebservice/user/UserController.java
+++ src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -1,7 +1,10 @@
 package com.example.restfulwebservice.user;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,12 +24,25 @@ public class UserController {
     // GET /users/1 or /users/10  -> String
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id) {
-        return service.findOne(id);
+        User user = service.findOne(id);
+
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        return user;
     }
 
     // POST
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@RequestBody User user) {
         User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest() // 사용자 요청 URI
+                .path("/{id}") // buildAndExpand를 통해 얻은 값이 들어옴
+                .buildAndExpand(savedUser.getId())  // {id}에 넣어줄 값
+                .toUri();   // URI 생성
+
+        return ResponseEntity.created(location).build();
     }
 }
