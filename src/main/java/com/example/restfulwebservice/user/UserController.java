415041a84ef69c2d593b0cff347b1661c79d208e
LEE_CHANG_HUN <chlee@kimzipsa.co.kr>
2022-02-06 18:40:30 +0900

    service 에 final 적용

--- src/main/java/com/example/restfulwebservice/user/UserController.java
+++ src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 public class UserController {
-    private UserDaoService service;
+    private final UserDaoService service;
 
     // 생성자를 통한 의존성 주입
     public UserController(UserDaoService service) {
